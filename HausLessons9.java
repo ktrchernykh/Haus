2ad739ba824b10f2e485970e6c3ebca80b710d42
ktrchernykh <115928326+ktrchernykh@users.noreply.github.com>
2022-11-25 15:26:39 +0100

    Add files via upload

---
+++ HausLessons9.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class HausLessons9 {
+    public static void main(String[] args) {
+        //First level: Level1:
+        //1) В программу (в консоль) вводится строка. Найдите длину строки, если она больше 10, то выведите true, иначе false
+        //2) В программу (в консоль) вводится строка. Если первый и последний символ равны - выведите true, иначе false
+
+
+        //Level2:
+        //В программу вводится строка из двух букв A и B, замените все буквы A на B, а все B на A. Например ABBA -> BAAB
+
+
+        Scanner scanner = new Scanner(System.in);
+        
+        String str = scanner.nextLine();
+        int len = str.length();
+
+        if (len > 10) {
+            System.out.println(true);
+        }
+        else {
+            System.out.println(false);
+        }
+
+    }
+}
