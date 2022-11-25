6e53722af919237db244f5af0c0e033dc358623d
ktrchernykh <115928326+ktrchernykh@users.noreply.github.com>
2022-11-25 15:57:57 +0100

    Add files via upload

--- HausLessons9.java
+++ HausLessons9.java
@@ -10,18 +10,31 @@ public class HausLessons9 {
         //Level2:
         //В программу вводится строка из двух букв A и B, замените все буквы A на B, а все B на A. Например ABBA -> BAAB
 
-
         Scanner scanner = new Scanner(System.in);
-        
+
         String str = scanner.nextLine();
         int len = str.length();
 
-        if (len > 10) {
+        if ( len > 10) {
             System.out.println(true);
         }
         else {
             System.out.println(false);
         }
+        char first = str.charAt(0);
+        char last = str.charAt(len-1);
 
+        if(first == last){
+            System.out.println(true);
+        }
+        else{
+            System.out.println(false);
+        }
     }
 }
+
+
+
+
+
+
